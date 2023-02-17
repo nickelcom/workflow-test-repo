7632a48ee945f62531f969a63802607f4db7d7c6
nickelcom <ntolmie@elcom.com>
2023-02-17 12:39:59 +0000

    Creating Animal class and adding implemetation for cat method makeNoise()

---
+++ Animal.java
@@ -0,0 +1,8 @@
+public abstract class Animal {
+
+    String name;
+    int age;
+
+    public abstract void makeNoise();
+
+}
---
+++ Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal{
+
+    public Cat (String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println("");
+    }
+}
